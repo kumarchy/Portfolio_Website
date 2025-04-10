2da9c791bf41dc88b6dd8b9b5ef82ce4a0571472
Kumar Chaudhary <115098236+kumarchy@users.noreply.github.com>
2025-04-10 09:28:42 +0530

    Update Resume.jsx

--- frontend/src/components/Resume/Resume.jsx
+++ frontend/src/components/Resume/Resume.jsx
@@ -5,7 +5,7 @@ import { FiDownload } from "react-icons/fi";
 const Resume = () => {
   return (
     <div className='resume-container'>
-    <a href="/Resume.pdf" download className='resume'>
+    <a href="/KUMAR_CHAUDHARY_RESUME.pdf" download className='resume'>
       Download Resume <FiDownload />
     </a>
   </div>
